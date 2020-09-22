9102953fd7230748fda3bfdd28c519e8950b4062
Michael B. Klein <mbklein@gmail.com>
2020-09-22 14:30:48 -0500

    Add the ability to inspect and kill running singleton tasks

--- lib/meadow/async.ex
+++ lib/meadow/async.ex
@@ -5,31 +5,69 @@ defmodule Meadow.Async do
 
   @environment Mix.env()
 
-  def run_once(task_id, fun) do
-    run_once(task_id, fun, @environment)
+  @doc """
+  Find a running singleton task with the given label
+  """
+  def find_task(label) do
+    case Meadow.TaskRegistry |> Registry.lookup(label) do
+      [{pid, _}] -> pid
+      _ -> nil
+    end
   end
 
-  def run_once(task_id, fun, :test) do
-    send(self(), {task_id, fun.()})
+  @doc """
+  Kill the singleton task with the given label
+  """
+  def kill!(label) do
+    case find_task(label) do
+      nil ->
+        {false, :not_found}
+
+      pid ->
+        Meadow.TaskRegistry |> Registry.unregister(label)
+        {Process.exit(pid, :kill), pid}
+    end
+  end
+
+  @doc """
+  List all running singleton tasks and their pids
+  """
+  def list_tasks do
+    Meadow.TaskRegistry
+    |> Registry.select([{{:"$1", :"$2", :"$3"}, [], [{{:"$1", :"$2", :"$3"}}]}])
+    |> Enum.map(fn {label, pid, _} -> {label, pid} end)
+  end
+
+  @doc """
+  Kick off a labeled singleton task. If a task with the same label is
+  already running, returns `{:running, pid}`. Otherwise, starts the
+  task and returns `{:ok, pid}`
+  """
+  def run_once(label, fun) do
+    run_once(label, fun, @environment)
+  end
+
+  def run_once(label, fun, :test) do
+    send(self(), {label, fun.()})
     {:sync, self()}
   end
 
-  def run_once(task_id, fun, _env) do
-    case Meadow.TaskRegistry |> Registry.lookup(task_id) do
-      [{pid, _}] ->
-        {:running, pid}
-
-      _ ->
+  def run_once(label, fun, _env) do
+    case find_task(label) do
+      nil ->
         receiver = self()
 
         Task.start(fn ->
           try do
-            Meadow.TaskRegistry |> Registry.register(task_id, nil)
-            send(receiver, {task_id, fun.()})
+            Meadow.TaskRegistry |> Registry.register(label, nil)
+            send(receiver, {label, fun.()})
           after
-            Meadow.TaskRegistry |> Registry.unregister(task_id)
+            Meadow.TaskRegistry |> Registry.unregister(label)
           end
         end)
+
+      pid ->
+        {:running, pid}
     end
   end
 end
--- test/meadow/async_test.exs
+++ test/meadow/async_test.exs
@@ -1,6 +1,8 @@
 defmodule Meadow.AsyncTest do
   use ExUnit.Case
 
+  import Assertions
+
   describe "sync (test) mode" do
     test "runs synchronously" do
       assert {:sync, _} =
@@ -38,6 +40,49 @@ defmodule Meadow.AsyncTest do
                )
 
       assert_receive({"test:123", {:running, pid}}, 1000)
+    end
+  end
+
+  describe "process management" do
+    setup do
+      [123, 456, 789]
+      |> Enum.map(fn id ->
+        Meadow.Async.run_once(
+          "test:#{id}",
+          fn -> :timer.sleep(:infinity) end,
+          :dev
+        )
+      end)
+
+      on_exit(fn ->
+        Meadow.Async.list_tasks()
+        |> Enum.each(fn {_, pid} -> Process.exit(pid, :kill) end)
+      end)
+
+      :timer.sleep(1)
+      :ok
+    end
+
+    test "list_tasks/0" do
+      assert_lists_equal(
+        Meadow.Async.list_tasks()
+        |> Enum.map(fn {id, _pid} -> id end),
+        ["test:123", "test:456", "test:789"]
+      )
+    end
+
+    test "find_task/1" do
+      assert Meadow.Async.find_task("test:000") |> is_nil()
+      assert Meadow.Async.find_task("test:123") |> is_pid()
+    end
+
+    test "kill!/1" do
+      assert {false, :not_found} == Meadow.Async.kill!("test:000")
+      assert {true, pid} = Meadow.Async.kill!("test:123")
+      assert is_pid(pid)
+      :timer.sleep(1)
+      assert {false, :not_found} == Meadow.Async.kill!("test:123")
+      assert Meadow.Async.list_tasks() |> length() == 2
     end
   end
 end
